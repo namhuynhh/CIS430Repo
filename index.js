a839283a5ee18812e89395f63959432cc7a8996e
Dieu Dang <ddang@asu.edu>
2019-04-26 10:12:39 -0700

    add javascript for camera

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 "use strict";
 
-		function toggle(icon, content) {
+var takePhotoButton
+var photoStatus
+var photoImage
+var cameraOptions
+var imageFilename
+var options
+
+/* wait until all phonegap/cordova is loaded then call onDeviceReady*/
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+
+    StatusBar.overlaysWebView(false); // force ios to show status bar
+    showHomeTab();
+
+    takePhotoButton = document.getElementById('takePhotoButtonId')
+    photoStatus = document.getElementById('photoStatusId')
+    photoImage = document.getElementById('photoImageId')
+    imageFilename = document.getElementById('imageFilenameId')
+    cameraOptions = document.getElementById('cameraOptionsId')
+
+    options = {
+        quality: 40,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        correctOrientation: true,
+        mediaType: Camera.MediaType.PICTURE,
+        encodingType: Camera.EncodingType.JPEG,
+        cameraDirection: Camera.Direction.BACK,
+        targetWidth: 300,
+        targetHeight: 400
+    }
+
+    cameraOptions.innerHTML = JSON.stringify(options, null, 2)
+}
+
+function takePhoto() {
+    navigator.camera.getPicture(photoSuccess, photoError, options)
+}
+
+function photoSuccess(imageURI) {
+    photoStatus.innerHTML = "Success!";
+    imageFilename.innerHTML = imageURI;
+    photoImage.src = imageURI
+}
+
+function photoError(errorMessage) {
+    photoStatus.innerHTML = "Failed: " + errorMessage;
+    imageFilename.innerHTML = "";
+}
+
+function toggle(icon, content) {
 			var currentTransform = icon.style.transform;
 			console.log(currentTransform);
 			let nextTransform = "";
